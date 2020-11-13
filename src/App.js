f50ee64f37967a3b52688a9421ea62b8e09ced21
Umar Khalid <umar.qur105@gmail.com>
2020-11-13 15:05:09 +0500

    Auto Deploying on Surge

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ExpenseTracker from "./components/ExpenseTracker";
 function App() {
 	return (
 		<>
+			<h1>Umar Khalid</h1>
 			<ExpenseTracker />
 		</>
 	);
--- src/components/ExpenseTracker.js
+++ src/components/ExpenseTracker.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "../App.css";
 import Header from "./Header";
 import IncomeExpense from "./IncomeExpense";
 import Balance from "./Balance";
